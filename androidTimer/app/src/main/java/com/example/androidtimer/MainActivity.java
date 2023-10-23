c00b9f175122140e4f28b7226e266e47386ad6cc
BlueKitsune狐 <saadshaan619@gmail.com>
2023-10-23 23:47:23 +0500

    improved timer

--- androidTimer/app/src/main/java/com/example/androidtimer/MainActivity.java
+++ androidTimer/app/src/main/java/com/example/androidtimer/MainActivity.java
@@ -2,44 +2,155 @@ package com.example.androidtimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar timerSeekBar;
+    TextView timerTextView;
+    Button controllerButton;
+    Boolean counterIsActive = false;
+    CountDownTimer countDownTimer;
+
+    public void resetTimer() {
+
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        countDownTimer.cancel();
+        controllerButton.setText("Go!");
+        timerSeekBar.setEnabled(true);
+        counterIsActive = false;
+
+    }
+
+    public void updateTimer(int secondsLeft) {
+
+        int minutes = (int) secondsLeft / 60;
+        int seconds = secondsLeft - minutes * 60;
+
+        String secondString = Integer.toString(seconds);
+
+        if (seconds <= 9) {
+
+            secondString = "0" + secondString;
+
+        }
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+
+    }
+
+
+    public void controlTimer(View view) {
+
+        if (counterIsActive == false) {
+
+            counterIsActive = true;
+            timerSeekBar.setEnabled(false);
+            controllerButton.setText("Stop");
+
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                    updateTimer((int) millisUntilFinished / 1000);
+
+                }
+
+                @Override
+                public void onFinish() {
+
+                    resetTimer();
+//                    MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                    MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.yamete_kudasai);
+                    mplayer.start();
+
+                }
+            }.start();
+
+        } else {
+
+            resetTimer();
+
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //this method will make the timer and then destroy itself after the time has reached;
-        new CountDownTimer(10000,1000){
-            public void onTick(long millisUntilFinished) {
-                Log.i("Ticked", String.valueOf(millisUntilFinished/1000));
-            }
+        timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+        timerTextView = (TextView) findViewById(R.id.timerTextView);
+        controllerButton = (Button) findViewById(R.id.controllerButton);
 
-            public void onFinish() {
-                Log.i("Tick finished", "the counter has finihed now starting the self destruction");
-            }
-        };
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
 
-
-        //this method will make the timer existing even when not in use;
-        Handler handler = new Handler();
-        Runnable run = new Runnable() {
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void run() {
-                //Code to run;
-                Log.i("Done","Counter has ticked");
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-                handler.postDelayed(this, 1000);
+                updateTimer(progress);
+
             }
-        };
 
-        handler.post(run);
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 }
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        //this method will make the timer and then destroy itself after the time has reached;
+//        new CountDownTimer(10000,1000){
+//            public void onTick(long millisUntilFinished) {
+//                Log.i("Ticked", String.valueOf(millisUntilFinished/1000));
+//            }
+//
+//            public void onFinish() {
+//                Log.i("Tick finished", "the counter has finihed now starting the self destruction");
+//            }
+//        };
+//
+//
+//        //this method will make the timer existing even when not in use;
+//        Handler handler = new Handler();
+//        Runnable run = new Runnable() {
+//            @Override
+//            public void run() {
+//                //Code to run;
+//                Log.i("Done","Counter has ticked");
+//
+//                handler.postDelayed(this, 1000);
+//            }
+//        };
+//
+//        handler.post(run);
+//
+//
+//    }
+//}
